7329245d11f48443c175c105925a0dddfadad112
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2024-01-17 05:38:40 +0100

    configuraction tailwindcss and autoprefixed, add postcssConfig

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div>
-      <h1>SimplyReactSpotify</h1>
+      <h1 className="bg-blue color-blue">SimplyReactSpotify</h1>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+modeule.exports = {
+  content: ['/index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
