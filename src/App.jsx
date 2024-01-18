c5d4a0ccdaffdc00426e1696ceecf76b338a73c9
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2024-01-18 10:31:55 +0100

    add imports, and API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+const API = '/assets/api.json';
+
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => setSongs(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <div>
       <h1 className="bg-blue color-blue">SimplyReactSpotify</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
