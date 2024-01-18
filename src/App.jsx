e62d19aae9f2a3860e60d719a7beb8738b9132db
Grzegorz Gardecki <gardecki.grzegorz@gmail.com>
2024-01-18 10:51:42 +0100

    delate bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from "react";
-
-const API = '/assets/api.json';
+import { useEffect, useState } from "react";
+const API = '/assets/api.json'
 
 function App() {
   const [songs, setSongs] = useState([]);
 
   useEffect(() => {
-    fetch(API)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
-      })
-      .then(data => setSongs(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+    fetch(API).then(response => {
+      if (response.ok){
+        response.json().then(response => setSongs(response));
+      }
+    })
+
+  },[]);
+
+  if (song.length < 1) {
+    return (<div>Loader</div>)
+  }
 
   return (
-    <div>
-      <h1 className="bg-blue color-blue">SimplyReactSpotify</h1>
-    </div>
-  );
+<div>
+  <section>  
+    <h1>Songs:</h1>
+    <ul>
+      {songs.map((song, index)=> (<li key={song.id}>
+        {song.title} by {song.author}
+      </li>))}
+    </ul>
+    </section>
+
+</div>
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-modeule.exports = {
-  content: ['/index.html', './src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+      extend: {},
   },
   plugins: [],
-}
-
+};
